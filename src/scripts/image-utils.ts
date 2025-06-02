46ceb8f5133b9f91528308712aecc718c3380529
Liks <82384131+WenLinkis@users.noreply.github.com>
2025-06-02 09:58:12 +0800

    refactor: modify image-utils

--- src/scripts/image-utils.ts
+++ src/scripts/image-utils.ts
@@ -1,8 +1,8 @@
 import type { ImageMetadata } from "astro";
 import path from "path";
-const images = import.meta.glob<{ default: ImageMetadata }>("@images/*");
+const images = import.meta.glob<{ default: ImageMetadata }>("@images/**");
 
-export async function getImportImage(imageName: string) {
-  const imagePath = path.join("/src/images", imageName);
-  return images[imagePath]();
+export function getImportImage(imagePath: string) {
+  const image = path.join("/src/images", imagePath);
+  return images[image]();
 }
