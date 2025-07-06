59fddbf1951ae268a89ff4288c53a22e178a52e5
Liks <82384131+WenLinkis@users.noreply.github.com>
2025-07-06 22:12:38 +0800

    feat(func): sort the post list by update date

--- src/scripts/content-utils.ts
+++ src/scripts/content-utils.ts
@@ -1,12 +1,22 @@
 import { getCollection, type CollectionKey } from "astro:content";
 import dayjs from "dayjs";
 
-export async function getSortedCollection(name: CollectionKey) {
+export async function getSortedCollection(
+  name: CollectionKey,
+  byUpdateDate: boolean = true,
+) {
   const collection = await getCollection(name);
 
   const sortedCollection = collection.sort((a, b) => {
-    const dateA = dayjs(a.data.publishDate);
-    const dateB = dayjs(b.data.publishDate);
+    const dateA =
+      byUpdateDate && a.data.updateDate !== undefined
+        ? dayjs(a.data.updateDate)
+        : dayjs(a.data.publishDate);
+    const dateB =
+      byUpdateDate && b.data.updateDate !== undefined
+        ? dayjs(b.data.updateDate)
+        : dayjs(b.data.publishDate);
+
     return dateA.isBefore(dateB) ? 1 : -1;
   });
 
